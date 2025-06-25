377d9a32cc4aaea2f2e3e7cfcdcbde8abc45c58b
molor824 <molormargaderdene@gmail.com>
2025-06-25 21:12:35 +0800

    Add tabs and project descriptions

---
+++ src/components/Tab.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { Component } from "svelte";
+
+  const {
+    tabs = {},
+  }: {
+    tabs?: Record<string, () => Component>;
+  } = $props();
+
+  let currentTab = $state<string>("");
+  let CurrentComponent = $derived(tabs[currentTab]?.());
+</script>
+
+<nav class="flex gap-4">
+  {#each Object.keys(tabs) as tab (tab)}
+    <button onclick={() => (currentTab = tab)}>{tab}</button>
+  {/each}
+</nav>
+
+<CurrentComponent></CurrentComponent>
