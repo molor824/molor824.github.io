99f4af8eed184bce4a4b6519dad5b184a705a595
molor824 <molormargaderdene@gmail.com>
2026-08-06 15:07:33 +0800

    Add color transition

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-/** @type{HTMLTemplateElement} */
-const projectTemplate = document.getElementById("project-template");
+const id = document.getElementById.bind(document);
+const query = document.querySelector.bind(document);
+const body = document.body;
+
+const btnColors = {
+  "btn-white": null,
+  "btn-red": { bg: "--red-bg-color", text: "--red-text-color" },
+  "btn-green": { bg: "--green-bg-color", text: "--green-text-color" },
+  "btn-blue": { bg: "--blue-bg-color", text: "--blue-text-color" },
+};
+
+Object.entries(btnColors).forEach(([btnId, props]) => id(btnId).addEventListener("click", () => {
+  if (props) {
+    body.style.setProperty("--background-color", `var(${props.bg})`);
+    body.style.setProperty("--text-color", `var(${props.text})`);
+  } else {
+    body.style.removeProperty("--background-color");
+    body.style.removeProperty("--text-color");
+  }
+}));
